ea1d4ff451f8fd6e12f06db6a5fc2a1f31659705
Marcos Parreiras <marcosfparreiras@gmail.com>
2016-02-12 17:34:26 -0200

    Split run method into smaller methods

--- nokogiri-example.rb
+++ nokogiri-example.rb
@@ -5,15 +5,44 @@ require 'restclient'
 class ComoMeSintoQuando
 
   def initialize(page_number = 1)
-    @page_number = page_number
+    page_limit = 1450
+    if(page_number <= 0)
+      @page_number = 1
+    elsif(page_number > page_limit)
+      @page_number = page_limit
+    else
+      @page_number = page_number
+    end
   end
 
   def url
-    "http://comoeumesintoquando.tumblr.com/#{@page_number}"
+    "http://comoeumesintoquando.tumblr.com/page/#{@page_number}"
   end
 
   def get_dom_elements
     Nokogiri::HTML(RestClient.get(url))
+  end
+
+  def get_gif(post)
+    {
+      :text => get_gif_title(post),
+      :img =>get_gif_url(post)
+    }
+  end
+
+  def get_gif_title(post)
+    post.css('div.post-content h3 a').text
+  end
+
+  def get_gif_url(post)
+    img_tag = post.css('div.post-content p img') #|| post.css('div.post-content figure img')
+    if(img_tag.empty?)
+      img_tag = post.css('div.post-content figure img')
+    end
+    if(img_tag.empty?)
+      img_tag = post.css('div.post-content img')
+    end
+    img_tag[0]['src']
   end
 
   def run
@@ -22,51 +51,16 @@ class ComoMeSintoQuando
 
     posts = page.css('div.post.post-type-text')
     posts.each do |post|
-      a_tag = post.css('div.post-content h3 a')
-      img_tag = post.css('div.post-content p img') #|| post.css('div.post-content figure img')
-      if(img_tag.empty?)
-        img_tag = post.css('div.post-content figure img')
-      end
-      if(img_tag.empty?)
-        img_tag = post.css('div.post-content img')
-      end
-
-      gif = Hash.new
-      gif[:text] = a_tag.text
-      gif[:img] = img_tag[0]['src']
-
-      gifs << gif
-
-      # puts a_tag.text
-      # puts img_tag[0]['src']
-      # puts "\n"
+      gifs << get_gif(post)
     end
-
     gifs
-
   end
-  # last_page = 1450
-  # page = Nokogiri::HTML(RestClient.get("http://comoeumesintoquando.tumblr.com"))
 
-  # posts = page.css('div.post.post-type-text')
-  # posts.each do |post|
-  #   a_tag = post.css('div.post-content h3 a')
-  #   img_tag = post.css('div.post-content p img') #|| post.css('div.post-content figure img')
-  #   if(img_tag.empty?)
-  #     img_tag = post.css('div.post-content figure img')
-  #   end
-  #   if(img_tag.empty?)
-  #     img_tag = post.css('div.post-content img')
-  #   end
-
-  #   puts a_tag.text
-  #   puts img_tag[0]['src']
-
-  #   puts "\n"
-  # end
 end
 
-example = ComoMeSintoQuando.new(0)
+
+
+example = ComoMeSintoQuando.new(2)
 
 gifs =  example.run
 gifs.each do |key, value|
