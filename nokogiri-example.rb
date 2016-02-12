ccc94c1a50af7e0d71d21d1098c750b3ad1dc670
Marcos Parreiras <marcosfparreiras@gmail.com>
2016-02-12 16:56:12 -0200

    Remove unused comments and debug printings

--- nokogiri-example.rb
+++ nokogiri-example.rb
@@ -1,36 +1,22 @@
-# require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 require 'restclient'
 
-# page = Nokogiri::HTML(RestClient.get("http://en.wikipedia.org/"))
 page = Nokogiri::HTML(RestClient.get("http://comoeumesintoquando.tumblr.com/"))
-# puts page.class   # => Nokogiri::HTML::Document
 
 posts = page.css('div.post.post-type-text')
 posts.each do |post|
   a_tag = post.css('div.post-content h3 a')
   img_tag = post.css('div.post-content p img') #|| post.css('div.post-content figure img')
   if(img_tag.empty?)
-    puts 'buscou de novo'
     img_tag = post.css('div.post-content figure img')
   end
   if(img_tag.empty?)
-    puts 'buscou de novo outra vez'
     img_tag = post.css('div.post-content img')
   end
 
   puts a_tag.text
   puts img_tag[0]['src']
-  # p a_tag
-  # p img_tag
 
-  # puts a_tag.class
-  # puts img_tag.class
-
-  puts "\n----------------------\n"
+  puts "\n"
 end
-
-# p content.css('h3 a').text
-# p content.css('p img')
-
