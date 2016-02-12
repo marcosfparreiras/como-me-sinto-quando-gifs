8cd64716828039f03fc8a197b81a8011d6e4b17d
Marcos Parreiras <marcosfparreiras@gmail.com>
2016-02-12 16:34:12 -0200

    Use iteration over Nokogiri element

--- nokogiri-example.rb
+++ nokogiri-example.rb
@@ -3,5 +3,16 @@ require 'nokogiri'
 require 'open-uri'
 require 'restclient'
 
-page = Nokogiri::HTML(RestClient.get("http://en.wikipedia.org/"))
+# page = Nokogiri::HTML(RestClient.get("http://en.wikipedia.org/"))
+page = Nokogiri::HTML(RestClient.get("http://comoeumesintoquando.tumblr.com/"))
 puts page.class   # => Nokogiri::HTML::Document
+
+content = page.css('div.post-content')
+content.each do |c|
+  puts c.css('h3 a').text
+  p c.css('p img').name
+end
+
+# p content.css('h3 a').text
+# p content.css('p img')
+
